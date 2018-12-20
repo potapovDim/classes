fe59f4e79d4ae69b97c5825c7f7e99909bde0fea
potapovDim <potapovd3012@gmail.com>
2018-12-20 10:02:45 +0200

    add looping, and exit factor

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,23 +5,30 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Вгадайте число!");
     let secret_number = rand::thread_rng().gen_range(1, 100);
+    loop {
+        println!("Вгадайте число!");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .ok()
-        .expect("Something wend wrong");
+        io::stdin()
+            .read_line(&mut guess)
+            .ok()
+            .expect("Something wend wrong");
 
-    let guess: u32 = guess.trim().parse().ok().expect("Please enter a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Your number is {}", guess);
 
-    println!("Your number is {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Your number is less than secret"),
-        Ordering::Greater => println!("Your number is great than secret"),
-        Ordering::Equal => println!("Sucess!!!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your number is less than secret"),
+            Ordering::Greater => println!("Your number is great than secret"),
+            Ordering::Equal => {
+                println!("Sucess!!!!");
+                break;
+            }
+        }
     }
 }
