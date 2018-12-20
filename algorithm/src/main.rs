8da40542ec3cf5dcc6161e5680314cbfafddaec1
potapovDim <potapovd3012@gmail.com>
2018-12-20 16:16:26 +0200

    range

--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
     let mut range = 0..10;
-
+    // iterators
     loop {
         match range.next() {
             Some(x) => println!("{}", x),
             None => break,
         }
     }
+    // consumers
+    let one_to_one_hundred = (0..100).collect::<Vec<i32>>();
 }
