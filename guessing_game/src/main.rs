6632bd34fed6fdbf3c54065fc24a006acd774dc0
potapovDim <potapovd3012@gmail.com>
2018-12-20 09:51:43 +0200

    rand lib, parse from stdin

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,27 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Вгадайте число!");
+    let secret_number = rand::thread_rng().gen_range(1, 100);
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .ok()
+        .expect("Something wend wrong");
+
+    let guess: u32 = guess.trim().parse().ok().expect("Please enter a number");
+
+    println!("Your number is {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Your number is less than secret"),
+        Ordering::Greater => println!("Your number is great than secret"),
+        Ordering::Equal => println!("Sucess!!!!"),
+    }
 }
