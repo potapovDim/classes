a1b94fdc19f2a608004e75cc1815b5411cc1ea5d
potapovDim <potapovd3012@gmail.com>
2018-12-21 08:36:19 +0200

    tread base

--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -8,5 +8,13 @@ fn main() {
         }
     }
     // consumers
-    let one_to_one_hundred = (0..100).collect::<Vec<i32>>();
+    // let one_to_one_hundred = (0..100).collect::<Vec<i32>>();
+    let greater_than_two = (0..10).find(|x| *x>2);
+    // fold -> js reduce
+    for i in &greater_than_two {
+        println!("{} greater ", i);
+    }
+    let sum = (0..20).fold(0, |acc, item| acc + item);
+
+    println!("{}", sum);
 }
---
+++ day2/src/main.rs
@@ -0,0 +1,8 @@
+use std::thread;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        "Hello from thread "
+    });
+    println!("{}", handle.join().unwrap());
+}
