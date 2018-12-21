32b1f6729ba9fa40cf48cdee56c07fad56d745c1
potapovDim <potapovd3012@gmail.com>
2018-12-21 12:44:32 +0200

    base tcp connection working

---
+++ web_server/src/main.rs
@@ -0,0 +1,18 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+        println!("Connection established!");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buff = [0; 512];
+    stream.read(&mut buff).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buff[..]));
+}
