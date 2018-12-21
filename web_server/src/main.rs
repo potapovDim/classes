e4135897e4a691454761ee8dc2265acfd1d78b21
potapovDim <potapovd3012@gmail.com>
2018-12-21 13:21:45 +0200

    update rust version

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -10,9 +11,15 @@ fn main() {
         println!("Connection established!");
     }
 }
-
 fn handle_connection(mut stream: TcpStream) {
-    let mut buff = [0; 512];
-    stream.read(&mut buff).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buff[..]));
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let contents =
+        fs::read_to_string("/Users/dpot/Documents/classes/web_server/src/index.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
