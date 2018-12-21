e014b6feddd7dca0d62018bbc8f5ea2f43d1c4f8
potapovDim <potapovd3012@gmail.com>
2018-12-21 08:51:31 +0200

    works as expected

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,8 @@
 use std::thread;
 use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 
-fn main() {
+fn example1() {
     let data = Arc::new(Mutex::new(vec![1u32, 2, 3]));
     for i in 0..3 {
         let data = data.clone();
@@ -12,3 +13,23 @@ fn main() {
     }
     thread::sleep_ms(200)
 }
+
+fn example2() {
+    let data = Arc::new(Mutex::new(0u32));
+    let (tx, rx) = mpsc::channel();
+
+    for _ in 0..10 {
+        let (data, tx) = (data.clone(), tx.clone());
+        thread::spawn(move || {
+            let mut data = data.lock().unwrap();
+            *data +=1;
+
+            tx.send(());
+        });
+    }
+
+    for _ in 0..10 {
+        rx.recv();
+    }
+}
+fn main() {}
