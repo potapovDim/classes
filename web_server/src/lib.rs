a2e346743bedd2e1c2b0b42d00fe1b1d5993a454
potapovDim <potapovd3012@gmail.com>
2018-12-22 13:48:52 +0200

    refactor thread poll

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,19 +1,24 @@
+use std::sync::mpsc;
 use std::thread;
 
 pub struct ThreadPool {
   workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0); // validation that required poll size contains more than 0 threads
+
+    let (sender, receiver) = mpsc::channel();
     let mut workers = Vec::with_capacity(size);
 
     for id in 0..size {
       workers.push(Worker::new(id));
     }
-
-    ThreadPool { workers }
+    ThreadPool { workers, sender }
   }
   // pub fn execute<F>(&self, _f: F)
   // where
