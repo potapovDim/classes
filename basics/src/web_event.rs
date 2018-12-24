d5c6d8b20359fea4691835a17b638a8ee7b0908a
potapovDim <potapovd3012@gmail.com>
2018-12-24 17:56:37 +0200

    need more information

---
+++ basics/src/web_event.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+enum WebEvent {
+  PageLoad,
+  Pageunload,
+  KeyPress(char),
+  Paste(String),
+  Click { x: i64, y: i64 },
+}
+
+fn inspect_web_element(event: WebEvent) {
+  match event {
+    WebEvent::KeyPress(c) => println!("Key {} was pressed ", c),
+    WebEvent::Paste(s) => println!("String was pasted {}", s),
+    WebEvent::PageLoad => println!("Page loading "),
+    WebEvent::Pageunload => println!("Page loaded"),
+    WebEvent::Click { x, y } => println!("Click was executed on x: {}, y: {}", x, y),
+  }
+}
