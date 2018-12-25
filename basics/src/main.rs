1ea10a46a44c1fc25d4beda87c27e30a6a532484
potapovDim <potapovd3012@gmail.com>
2018-12-25 19:04:03 +0200

    by cart

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,4 +1,4 @@
-fn is_divisible_by(or_num: i32, divisible: i32) -> bool {
+fn _is_divisible_by(or_num: i32, divisible: i32) -> bool {
   if or_num == 0 {
     return false;
   }
@@ -7,11 +7,12 @@ fn is_divisible_by(or_num: i32, divisible: i32) -> bool {
 
 fn _fizzbuzz_question(num_fb: i32) {
   for num in 1..num_fb + 1 {
-    if is_divisible_by(num, 3) && is_divisible_by(num, 5) {
+    let (by_three, by_five) = (_is_divisible_by(num, 3), _is_divisible_by(num, 5));
+    if by_five && by_three {
       println!("fizzbuzz");
-    } else if is_divisible_by(num, 3) {
+    } else if by_three {
       println!("fizz");
-    } else if is_divisible_by(num, 5) {
+    } else if by_five {
       println!("buzz");
     } else {
       println!("qua qua qua");
