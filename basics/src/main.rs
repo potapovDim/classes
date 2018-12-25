19d0b50345e6a942634ba5082f5fd9c9fe4a876b
potapovDim <potapovd3012@gmail.com>
2018-12-25 19:01:19 +0200

    fizzbuzz

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,10 +1,17 @@
+fn is_divisible_by(or_num: i32, divisible: i32) -> bool {
+  if or_num == 0 {
+    return false;
+  }
+  or_num % divisible == 0
+}
+
 fn _fizzbuzz_question(num_fb: i32) {
   for num in 1..num_fb + 1 {
-    if num % 3 == 0 && num % 5 == 0 {
+    if is_divisible_by(num, 3) && is_divisible_by(num, 5) {
       println!("fizzbuzz");
-    } else if num % 3 == 0 {
+    } else if is_divisible_by(num, 3) {
       println!("fizz");
-    } else if num % 5 == 0 {
+    } else if is_divisible_by(num, 5) {
       println!("buzz");
     } else {
       println!("qua qua qua");
